afffdd59cff8b5d702ec6c9260336cbc2c30a700
Robin Garcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2016-11-08 12:05:24 +0100

    Add example file of cellcycle from SBToolbox2

---
+++ matlab_scripts/init_script.m
@@ -0,0 +1,6 @@
+%% Personal MATLAB-Init Script
+clear all;
+clc all;
+cd;
+% Set to MATLAB Folder
+cd /~MATLAB
---
+++ matlab_scripts/scriptIQM.m
@@ -0,0 +1,20 @@
+%% In this section the purpose of the script should be described.
+% Purpose of activity:
+%
+% Required previous scripts to be run:
+%
+% Author:
+%
+% Date:
+%
+
+%% Installation of IQM Tools
+% This block should always be on the top of every analysis script with IQM
+% Tools. It might only be ommitted in the case that a central installation
+% of IQM Tools is present on the computer system and the IQM Tools are
+% automatically loaded during the startup of MATLAB.
+clc;                        % Clear command window
+clear all;                  % Clear workspace from all defined variables
+close all;                  % Close all figures
+fclose all;                 % Close all open files
+restoredefaultpath();       % Clear all user installed toolboxes
