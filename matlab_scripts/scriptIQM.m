4829f764ddbdfd9221a67fda6aa58bf669676465
Robin Garcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2016-11-08 12:16:52 +0100

    Intro copied from sriptTemplateIQM.m

--- matlab_scripts/scriptIQM.m
+++ matlab_scripts/scriptIQM.m
@@ -18,3 +18,23 @@ clear all;                  % Clear workspace from all defined variables
 close all;                  % Close all figures
 fclose all;                 % Close all open files
 restoredefaultpath();       % Clear all user installed toolboxes
+
+% In the next line, please enter the path to your IQM Tools folder. It can
+% be a relative or an absolute path.
+PATH_IQM            = 'D:\IQM Tools Suite';
+oldpath             = pwd(); cd(PATH_IQM); installIQMtoolsInitial; cd(oldpath);
+
+%% "Initialize the compliance mode".
+% IQM Tools allows to automatically generate logfiles for all output that
+% is generated using the functions "IQMprintFigure" and
+% "IQMwriteText2File". These logfiles contain the username, the name of the
+% generated file (including the path), and the scripts and functions that
+% have been called to generate the output file. In order to ensure this is
+% working correctly, the only thing that needs to be done is to execute the
+% following command at the start of each analysis script.
+
+% The input argument to the "IQMinitializeCompliance" function needs to be
+% the name of the script file.
+IQMinitializeCompliance('TEMPLATE_SCRIPT_NAME');
+
+%% Here your code starts
