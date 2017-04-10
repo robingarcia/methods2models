4aa5f01ab0183fc365501e1b453405599636901d
robingarcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-04-10 08:53:10 +0200

    plot3 added

--- matlab_scripts/dataset_firstcheck.m
+++ matlab_scripts/dataset_firstcheck.m
@@ -1,5 +1,7 @@
 %% Plot your dataset
 %load('20170329T225509.mat');
+x = input('Input first protein');
+y = input('Input second protein');
 
 for i = 1:31
     figure(1)
@@ -7,6 +9,7 @@ for i = 1:31
     subplot(6,6,i)
     %s(i)=scatter(measurement{1,1}(i,:), measurement{1,1}(32,:))
     s(i)=scatter(mydata(i,:), mydata(32,:));
+    plot3(mydata(x,:), mydata(y,:), mydata(32,:));
 end
 
 
