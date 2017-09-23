054537dd7843f2d7c35f417b4c3b0172d6801528
robingarcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-09-23 02:49:57 +0200

    Batch file adapted. File will resume if fail detected.

--- batch/ex_batch.m
+++ batch/ex_batch.m
@@ -1,5 +1,5 @@
 %% Submit batch job
-addpath(genpath('~/methods2models'));
+addpath(genpath('~/methods2models/batch'));
 clust=parcluster('local');
 N=2; % for a quad-core computer
 job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Pool', N-1);
@@ -7,14 +7,19 @@ job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Po
 %% Query state of the job
 disp(get(job,'State'))
 
+
 %% Retrieve and process results
-results= fetchOutputs(job);
+if getReport(job.Tasks(1).Error)
+   disp(getReport(job.Tasks(1).Error))
+   job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Pool', N-1); 
+else
+   results= fetchOutputs(job);
+   disp(results)
+end
 % a= results{1};
 % hist(a)
 
 %% Delete job
 % delete(job)
-
-
 wait(job(end));
 exit;
