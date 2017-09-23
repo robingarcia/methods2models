19770dd619189a27124c23c8d686f263ee355cce
robingarcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-09-23 02:15:21 +0200

    Adapt bash script

--- m2m.sh
+++ m2m.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-matlab -nodisplay -nosplash -r "run ex_batch" > outfile.txt& # < /dev/null &
+nohup matlab -nodisplay -nosplash -r "run ex_batch" > outfile.txt </dev/null 2>&1  & #completely detached form terminal
