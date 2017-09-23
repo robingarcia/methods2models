aa93e937c379d2d2a76df5cc12cb4028d45c46bb
robingarcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-09-23 03:24:00 +0200

    batch file moved

--- ex_batch.m
+++ ex_batch.m
@@ -1,5 +1,5 @@
 %% Submit batch job
-addpath(genpath('~/methods2models/batch'));
+addpath(genpath('~/methods2models/'));
 clust=parcluster('local');
 N=2; % for a quad-core computer
 job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Pool', N-1);
@@ -9,9 +9,10 @@ disp(get(job,'State'))
 
 
 %% Retrieve and process results
+wait(job(end));
 if getReport(job.Tasks(1).Error)
    disp(getReport(job.Tasks(1).Error))
-   job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Pool', N-1); 
+   job = batch(clust, @m2m, 1, {0:2000,1000,2,0.01,'model_toettcher2008MEX',0}, 'Pool', N-1);
 else
    results= fetchOutputs(job);
    disp(results)
@@ -21,5 +22,4 @@ end
 
 %% Delete job
 % delete(job)
-wait(job(end));
 exit;
