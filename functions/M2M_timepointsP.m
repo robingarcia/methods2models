c3ab182364a5b425b2ef8fe391543a7cb7738697
robingarcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-10-27 20:43:38 +0200

    Generate some plots

--- functions/M2M_timepointsP.m
+++ functions/M2M_timepointsP.m
@@ -1,5 +1,5 @@
 function M2M_timepointsP(p_value,samples)
-%UNTITLED9 Summary of this function goes here
+%This function generates a plot of the inversion method
 %   Detailed explanation goes here
 %% Single timepoints via inversion method
 f = figure;
@@ -9,18 +9,18 @@ p.FontSize = 12;
 p.FontWeight = 'bold';
 
 subplot(1,2,1,'Parent',p)
-histogram(p_value(:,1));
+histogram(p_value(:,1),20);
 grid on
-title('Uniform distribution')
+title('Uniform distribution','Interpreter','latex')
 ylabel('Number of cells')
 
 subplot(1,2,2,'Parent',p)
-histogram(samples(:,1));
+histogram(samples(:,1),20);
 grid on
 title('New distribution $$f(a)=2 \gamma e^{- \gamma a}$$','Interpreter','latex')
 xlabel('Periods (h)')
 ylabel('Number of cells')
-matlab2tikz( 'inversion.tex', 'height', '\fheight', 'width', '\fwidth','floatFormat','%.3g' )
+% matlab2tikz( 'inversion.tex', 'height', '\fheight', 'width', '\fwidth','floatFormat','%.3g' )
 %% Uniform distribution
 histogram(p_value(:,1));
 grid on
